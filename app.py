9c1f911c22271065afae40839e08881a05736a82
22121 <22121@wc.school.nz>
2026-02-25 14:22:57 +1300

    Added database and done first query/connection functionality

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
 
+DATABASE = "cafe.db"
 
+def connect_database(db_file):
+    """
+    Creates a connection with the database
+    :param db_file:
+    :return:
+    """
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+        print("An error occured when connecting to the database")
+        return None
 @app.route('/')
 def render_homepage():
     return render_template('home.html')
@@ -10,6 +26,12 @@ def render_homepage():
 
 @app.route('/menu')
 def render_menu_page():
+    con = connect_database(DATABASE)
+    query = "SELECT name, description, volume, image, price FROM products"
+    cur = con.cursor()
+    cur.execute(query)
+    product_list = cur.fetchall()
+    con.close()
     return render_template('menu.html')
 
 
